a7e7100f881af931dd3e80d170104407fca2bfb7
David Vargas <dvargas92495@gmail.com>
2023-04-06 12:58:54 -0400

    Integrating Clerk with chrome extension on install

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,126 +1,113 @@
 import React from "react";
-import { Button, InputGroup, Label } from "@blueprintjs/core";
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignUp,
+  ClerkProvider,
+} from "@clerk/chrome-extension";
+import { useNavigate, Routes, Route, MemoryRouter } from "react-router-dom";
+
+const publishableKey = process.env.CLERK_PUBLISHABLE_KEY || "";
 
 // TODO - look into clerking this
-const App = () => {
-  const [isSignUp, setIsSignUp] = React.useState(false);
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
+const AppRoutes = () => {
+  const navigate = useNavigate();
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        width: "100%",
-        padding: "32px 0",
-        height: "100%",
-      }}
+    <ClerkProvider
+      publishableKey={publishableKey}
+      navigate={(to) => navigate(to)}
     >
-      <style>{`html, body, #app { height: 100%; }`}</style>
-      <div style={{ height: 160, width: 160 }}>
-        <img
-          src="https://samepage.network/images/logo.png"
-          height={160}
-          width={160}
-        />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "100%",
+          padding: "32px 0",
+          height: "100%",
+        }}
+      >
+        <style>{`html, body, #app { height: 100%; }`}</style>
+        <div style={{ height: 160, width: 160 }}>
+          <img
+            src="https://samepage.network/images/logo.png"
+            height={160}
+            width={160}
+          />
+        </div>
+        <h1>Welcome to SamePage</h1>
+        <div style={{ maxWidth: 480 }}>
+          <Routes>
+            <Route
+              path="/sign-up/*"
+              element={
+                <>
+                  <p className="mb-8" style={{ marginBottom: 32 }}>
+                    If you don't have a SamePage account, sign up to get
+                    started.
+                  </p>
+                  <SignUp signInUrl="/" afterSignInUrl={"/"} />
+                </>
+              }
+            />
+            <Route
+              path="/"
+              element={
+                <>
+                  <SignedIn>
+                    <p className="mb-8" style={{ marginBottom: 32 }}>
+                      You're successfully logged in to SamePage on this browser!
+                      You can navigate to a SamePage supported application to
+                      get started:
+                    </p>
+                    <div
+                      style={{
+                        display: "grid",
+                        gridTemplateColumns: "repeat(3, minmax(0, 1fr))",
+                        gap: "3rem",
+                      }}
+                      className="grid gap-12 grid-cols-3"
+                    >
+                      <a
+                        href="https://notion.so"
+                        className="flex flex-col"
+                        style={{ display: "flex", flexDirection: "column" }}
+                      >
+                        <img
+                          src={
+                            "https://samepage.network/images/apps/notion.png"
+                          }
+                          className="w-12 h-12"
+                          style={{ width: 48, height: 48 }}
+                        />
+                        <span>Notion</span>
+                      </a>
+                    </div>
+                  </SignedIn>
+                  <SignedOut>
+                    <p className="mb-8" style={{ marginBottom: 32 }}>
+                      If you have a SamePage account, sign in to get started.
+                    </p>
+                    <SignIn afterSignInUrl={"/"} signUpUrl={"/sign-up"} />
+                  </SignedOut>
+                </>
+              }
+            />
+          </Routes>
+        </div>
+        <div style={{ flexGrow: 1 }} />
+        <div>SamePage version {chrome.runtime.getManifest().version}</div>
       </div>
-      <h1>Welcome to SamePage</h1>
-      {isSignUp ? (
-        <>
-          <p>If you have a SamePage account, sign in to get started.</p>
-          <Label className={"w-1/2"}>
-            Email
-            <InputGroup
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              autoFocus
-              type={"text"}
-              name={"email"}
-              style={{ maxWidth: 320 }}
-            />
-          </Label>
-          <Label className={"w-1/2"}>
-            Password
-            <InputGroup
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              type={"password"}
-              name={"password"}
-              style={{ maxWidth: 320 }}
-            />
-          </Label>
-          <Button
-            style={{
-              padding: "4px 32px",
-              marginBottom: 32,
-            }}
-            intent={"primary"}
-          >
-            Sign In
-          </Button>
-          <p>Don't have an account yet?</p>
-          <p
-            style={{
-              color: "skyblue",
-              cursor: "pointer",
-              textDecoration: "underline",
-            }}
-            onClick={() => setIsSignUp(true)}
-          >
-            Create your SamePage account
-          </p>
-        </>
-      ) : (
-        <>
-          <p>If you don't have a SamePage account, sign up to get started.</p>
-          <Label className={"w-1/2"}>
-            Email
-            <InputGroup
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              autoFocus
-              type={"text"}
-              name={"email"}
-              style={{ maxWidth: 320 }}
-            />
-          </Label>
-          <Label className={"w-1/2"}>
-            Password
-            <InputGroup
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              type={"password"}
-              name={"password"}
-              style={{ maxWidth: 320 }}
-            />
-          </Label>
-          <Button
-            style={{
-              padding: "4px 32px",
-              marginBottom: 32,
-            }}
-            intent={"primary"}
-          >
-            Sign Up
-          </Button>
-          <p>Already have an account?</p>
-          <p
-            style={{
-              color: "skyblue",
-              cursor: "pointer",
-              textDecoration: "underline",
-            }}
-            onClick={() => setIsSignUp(true)}
-          >
-            Click here to log in
-          </p>
-        </>
-      )}
+    </ClerkProvider>
+  );
+};
 
-      <div style={{ flexGrow: 1 }} />
-      <div>SamePage version {chrome.runtime.getManifest().version}</div>
-    </div>
+const App = () => {
+  return (
+    <MemoryRouter>
+      <AppRoutes />
+    </MemoryRouter>
   );
 };
 
